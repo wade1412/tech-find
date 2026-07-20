4d9041fd4e80039be553e92596acc9a71cebc241
Valeriy Petrenko <fourcher00@gmail.com>
2026-07-20 09:00:05 -0400

    Manage Users page layout

--- src/pages/ManageServicesPage.tsx
+++ src/pages/ManageServicesPage.tsx
@@ -1,5 +1,20 @@
+import SectionHeader from "../features/technician-management/ui/SectionHeader";
+
 function ManageServicesPage() {
-  return <section>ManageServicesPage</section>;
+  return (
+    <div className="mx-auto max-w-6xl p-4 md:p-6">
+      <div className="flex flex-col gap-10">
+        <SectionHeader
+          label="Manage Services"
+          subtext="Edit units, brand and service zones"
+        />
+
+        <span className="text-base text-zinc-300 text-center">
+          Work In Progress, come back later
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default ManageServicesPage;
--- src/pages/ManageUsersPage.tsx
+++ src/pages/ManageUsersPage.tsx
@@ -1,5 +1,89 @@
+import { useSearchParams } from "react-router";
+import {
+  buttonContainerStyle,
+  centeredContainerStyle,
+  formStyle,
+  manageListGridStyle,
+  pageTitleWithButtonsContainerStyle,
+  secondaryButton,
+} from "../shared/styles/styles";
+import PageHeader from "../shared/ui/PageHeader";
+import SearchInput from "../shared/ui/SearchInput";
+import { AnimatePresence, motion } from "motion/react";
+import { technicianListVariants } from "../shared/styles/motionVariants";
+
 function ManageUsersPage() {
-  return <section>ManageUsersPage</section>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchTerm = searchParams.get("query") || "";
+
+  const handleInputChange = (value: string) => {
+    setSearchParams(
+      (prev) => {
+        const nextParams = new URLSearchParams(prev);
+
+        if (value) {
+          nextParams.set("query", value);
+        } else {
+          nextParams.delete("query");
+        }
+
+        return nextParams;
+      },
+      { replace: true },
+    );
+  };
+
+  return (
+    <div className={centeredContainerStyle}>
+      <section className={formStyle}>
+        <div className={pageTitleWithButtonsContainerStyle}>
+          <PageHeader
+            title="Manage Users"
+            subtitle="Select a user to edit the data and role"
+          />
+
+          <div className={buttonContainerStyle}>
+            <button className={secondaryButton}>Archived users</button>
+            <button className={secondaryButton}>Create User</button>
+          </div>
+        </div>
+
+        {/* Divider */}
+        <div
+          aria-hidden="true"
+          className="h-px w-full bg-zinc-200 dark:bg-zinc-800"
+        />
+
+        <div className={`${formStyle} px-2`}>
+          <div className="flex flex-col gap-2.5 sm:flex-row sm:items-center sm:justify-between">
+            <div className="w-full sm:w-auto sm:min-w-75">Control</div>
+
+            <SearchInput
+              placeholder="Search by name, ZIP, or zone..."
+              ariaLabel="Search technicians"
+              className="w-full sm:w-72"
+              value={searchTerm}
+              onValueChange={handleInputChange}
+            />
+          </div>
+
+          {/* Users List */}
+          <div>
+            <motion.div
+              className={manageListGridStyle}
+              variants={technicianListVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <AnimatePresence mode="popLayout">Users List</AnimatePresence>
+            </motion.div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default ManageUsersPage;
