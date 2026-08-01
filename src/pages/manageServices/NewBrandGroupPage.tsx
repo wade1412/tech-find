cbe5a8ae5c4a4815ce12088e16ec3431ed82870e
Valeriy Petrenko <fourcher00@gmail.com>
2026-08-01 06:22:32 -0400

    Feature: New brand group and edit brand group pages

--- src/pages/manageServices/NewBrandGroupPage.tsx
+++ src/pages/manageServices/NewBrandGroupPage.tsx
@@ -1,5 +1,20 @@
+import BrandGroupForm from "../../features/services-management/manage-brands/ui/BrandGroupForm";
+import { centeredContainerStyle, formStyle } from "../../shared/styles/styles";
+import PageHeader from "../../shared/ui/PageHeader";
+
 function NewBrandGroupPage() {
-  return <div>NewBrandGroupPage</div>;
+  return (
+    <div className={centeredContainerStyle}>
+      <section className={formStyle}>
+        <PageHeader
+          title="New Brand Group"
+          subtitle="Create a group used to organize brands in service filters"
+        />
+
+        <BrandGroupForm />
+      </section>
+    </div>
+  );
 }
 
 export default NewBrandGroupPage;
